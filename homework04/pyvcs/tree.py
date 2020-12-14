49ec7648d90ffa2544a51ddbf88646790f3f446a
Kormanovskaya Darina <darina.kormanovskaya@gmail.com>
2020-12-14 18:29:33 +0300

    add write_tree

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -1,28 +1,39 @@
+import hashlib
 import os
 import pathlib
 import stat
 import time
 import typing as tp
 
-import hashlib
-
 from pyvcs.index import GitIndexEntry, read_index
 from pyvcs.objects import hash_object
 from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 
 
+def gie_from_list_in_dir(gie_list: tp.List[GitIndexEntry], dirname: str) -> tp.List[GitIndexEntry]:
+    gies_in_dir: tp.List[GitIndexEntry] = []
+    for gie in gie_list:
+        if gie.name.find(dirname) == 0:
+            gies_in_dir.append(gie)
+    return gies_in_dir
+
+
 def write_tree(gitdir: pathlib.Path, index: tp.List[GitIndexEntry], dirname: str = "") -> str:
-    tree_entries : tp.List[bytes] = []
+    tree_entries = b""
     for gie in index:
-        
-        row = str(f"{gie.mode:o}") + " " + gie.name + "\0"
-        tree_entries.append(row.encode() + gie.sha1)
-
-    all_objects = b""
-    for entry in tree_entries:
-        all_objects += entry
-
-    return hash_object(all_objects, "tree", write=True)
+        current_place = gie.name[len(dirname) :]
+        name_parts = current_place.split("/")
+        if len(name_parts) != 1:
+            new_dirname = dirname + name_parts[0] + "/"
+            row = "40000 " + name_parts[0] + "\0"
+            dir_sha = write_tree(
+                gitdir, gie_from_list_in_dir(index, new_dirname), dirname=new_dirname
+            )
+            tree_entries += row.encode() + bytes.fromhex(dir_sha)
+        else:
+            row = str(f"{gie.mode:o}") + " " + name_parts[0] + "\0"
+            tree_entries += row.encode() + gie.sha1
+    return hash_object(tree_entries, "tree", write=True)
 
 
 def commit_tree(
