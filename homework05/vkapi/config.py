07d1793009bfbd70ed122cdc08483ecae38fbb3a
Darina Kormanovskaya <71320692+pappl-b@users.noreply.github.com>
2021-01-14 20:38:17 +0300

    Update config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "b7bf70d5a187b14ed67c529b71d91e3f0983ebe2f19bd37f55b9856d1fbd8a4a11f17e109fb1f0410fb89",
+    "access_token": "",
     "version": "5.126",
 }
