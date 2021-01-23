e1996a9258f55b131ea42af0e06e57c90fca74b9
Kormanovskaya Darina <darina.kormanovskaya@gmail.com>
2021-01-23 13:22:07 +0300

    working?

--- exam-winter/map.py
+++ exam-winter/map.py
@@ -18,13 +18,12 @@ class labyrinth:
                     nline.append(element)
                     if element == IS_VISITED:
                         self.start = (i, j)
-                    elif element == BOUND:
+                    elif element == DESTINATION:
                         self.finale = (i, j)
                     j += 1
                 self.map.append(nline)
         self.rows = len(self.map)
         self.cols = len(self.map[0])
-
     def __repr__(self):
         result = ""
         for i, line in enumerate(self.map):
@@ -51,40 +50,32 @@ class labyrinth:
             for j in range(self.cols):
                 visited[i].append(False)
 
-        self.print_solved(
-            self.step(visited, self.start[1], self.start[0], cur_path=[], min_path=[], min_dist=-1)
-        )
+        self.min_path = []
+        self.min_dist = -1
+        self.step(visited, self.start[1], self.start[0], cur_path=[])
 
-    def step(
-        self,
-        visited: tp.List[tp.List[bool]],
-        i: int,
-        j: int,
-        cur_path,
-        min_path,
-        min_dist: int,
-    ):
+        self.print_solved()
+
+    def step(self, visited: tp.List[tp.List[bool]], i: int, j: int, cur_path):
         if (i, j) == self.finale:
-            if len(cur_path) <= min_dist or min_dist == -1:
-                min_path.clear()
-                min_path.extend(cur_path)
-                min_path.append((j, i))
-                min_dist = len(min_path)
-            return min_path
+            if len(cur_path) <= self.min_dist or self.min_dist == -1:
+                self.min_path.clear()
+                self.min_path = cur_path.copy()
+                self.min_dist = len(self.min_path)
+            return
 
         visited[i][j] = True
         cur_path.append((j, i))
-
         possible_paths = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
         for path in possible_paths:
             if self.isValid(path[0], path[1]) and self.isSafe(visited, path[0], path[1]):
-                self.step(visited, path[0], path[1], cur_path, min_path, min_dist)
+                self.step(visited, path[0], path[1], cur_path)
         visited[i][j] = False
         cur_path.pop()
 
-    def print_solved(self, min_path):
+    def print_solved(self):
         new_lab = self.map.copy()
-        for path in min_path:
+        for path in self.min_path:
             new_lab[path[0]][path[1]] = IS_VISITED
         result = ""
         for i, line in enumerate(new_lab):
