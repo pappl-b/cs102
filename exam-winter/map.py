db84ea64fd13b3b29a4fc08d1a9ceebed8540516
Kormanovskaya Darina <darina.kormanovskaya@gmail.com>
2021-01-23 12:47:53 +0300

    smth

--- exam-winter/map.py
+++ exam-winter/map.py
@@ -1,11 +1,13 @@
 import sys
+import typing as tp
 
-CAN_GO = '.'
-VISITED = '☺'
-BOUND = '☒'
-DESTINATION = '☼'
+CAN_GO = "."
+IS_VISITED = "☺"
+BOUND = "☒"
+DESTINATION = "☼"
 
-class labyrinth():
+
+class labyrinth:
     def __init__(self, filename):
         self.map = []
         with open(filename, "r") as f:
@@ -14,12 +16,14 @@ class labyrinth():
                 j = 0
                 for element in line.rstrip():
                     nline.append(element)
-                    if element == VISITED:
-                        self.start = (i,j)
+                    if element == IS_VISITED:
+                        self.start = (i, j)
                     elif element == BOUND:
-                        self.finale = (i,j)
+                        self.finale = (i, j)
                     j += 1
                 self.map.append(nline)
+        self.rows = len(self.map)
+        self.cols = len(self.map[0])
 
     def __repr__(self):
         result = ""
@@ -30,9 +34,70 @@ class labyrinth():
                 result += "\n"
         return result
 
-def main():
+    def possible_to_go(self, x: int, y: int, visited: tp.List[tp.List[bool]]):
+        if (
+            0 <= x < self.cols
+            and 0 <= y < self.rows
+            and self.map[x][y] != BOUND
+            and visited[x][y] != True
+        ):
+            return True
+        return False
+
+    def solve(self):
+        visited = []
+        for i in range(self.rows):
+            visited.append([])
+            for j in range(self.cols):
+                visited[i].append(False)
+
+        print(
+            self.step(visited, self.start[1], self.start[0], cur_path=[], min_path=[], min_dist=-1)
+        )
+
+    def step(
+        self,
+        visited: tp.List[tp.List[bool]],
+        i: int,
+        j: int,
+        cur_path,
+        min_path,
+        min_dist: int,
+    ):
+        if (i, j) == self.finale:
+            if len(cur_path) < min_dist or min_dist == -1:
+                min_path.clear()
+                min_path.extend(cur_path)
+                min_path.append((j, i))
+                min_dist = len(min_path)
+            return min_path
+
+        visited[i][j] = True
+        cur_path.append((j, i))
+
+        possible_paths = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+        for path in possible_paths:
+            if self.possible_to_go(path[0], path[1], visited):
+                self.step(visited, path[0], path[1], cur_path, min_path, min_dist)
+
+        self.print_solved(min_path)
+        visited[i][j] = False
+        cur_path.pop()
+
+    def print_solved(self, min_path):
+        new_lab = self.map.copy()
+        for path in min_path:
+            new_lab[path[0]][path[1]] = IS_VISITED
+        result = ""
+        for i, line in enumerate(new_lab):
+            for char in line:
+                result += char
+            if i < len(new_lab) - 1:
+                result += "\n"
+        print(result)
+
+
+if __name__ == "__main__":
     given_filename = sys.argv[1]
     a = labyrinth(filename=given_filename)
-    print(a)
-
-main()
+    a.solve()
