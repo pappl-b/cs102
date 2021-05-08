a030c15aacc1843c2bcfacf06da8f65d4944fff7
Kormanovskaya Darina <darina.kormanovskaya@gmail.com>
2021-05-08 11:17:15 +0300

    formatted

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,6 +1,4 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+from bottle import route, run, template, request, redirect
 
 from scrapper import get_news
 from db import News, session
@@ -11,7 +9,7 @@ from bayes import NaiveBayesClassifier
 def news_list():
     s = session()
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template("news_template", rows=rows)
 
 
 @route("/add_label/")
@@ -29,8 +27,8 @@ def update_news():
 @route("/classify")
 def classify_news():
     # PUT YOUR CODE HERE
+    pass
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
