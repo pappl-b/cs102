c2dff88c1a4146a0b3b4e9cc7c995d59d3576d02
Kormanovskaya Darina <darina.kormanovskaya@gmail.com>
2021-10-14 13:05:30 +0300

    trying to simplify

--- homework09-web/app.py
+++ homework09-web/app.py
@@ -1,74 +1,35 @@
 import datetime as dt
-import json
-import re
 import typing as tp
 
-import jwt
-from slowapi import JsonResponse, Request, Response, SlowAPI
+from slowapi import JsonResponse, SlowAPI, Request
 from slowapi.middlewares import CORSMiddleware
+from slowapi.security import Security
 
-app = SlowAPI()
+application = SlowAPI()
 notes: tp.Dict[int, tp.Dict[str, tp.Any]] = {}
-users: tp.Set[str] = set()
-
 JWT_SECRET = "secret"
 JWT_ALGORITHM = "HS256"
 JWT_EXP_DELTA_SECONDS = 300
+users = Security(JWT_SECRET, JWT_ALGORITHM, JWT_EXP_DELTA_SECONDS)
 
 
-def authorization_required(route: tp.Callable):
-    validation_re = r"^Bearer ([A-Za-z0-9]+\.[A-Za-z0-9]+\..*)$"
-    invalid_response = Response(401, body=b"Invalid credentials")
-
-    def check_auth(request: Request, *args, **kwargs):
-        credentials = request.headers.get("Authorization", None)
-        if credentials is None:
-            return invalid_response
-
-        find_token = re.search(validation_re, credentials.strip())
-        if find_token is None:
-            return invalid_response
-
-        try:
-            payload = jwt.decode(find_token.group(1), JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        except jwt.PyJWTError:
-            return invalid_response
-
-        if payload["email"] not in users:
-            return invalid_response
-
-        return route(request, *args, **kwargs)
-
-    return check_auth
-
-
-def dt_json_serializer(o: tp.Any):
+def dt_json_serializer(o):
     if isinstance(o, (dt.date, dt.datetime)):
         return o.isoformat()
 
 
-@app.post("/api/jwt-auth")
-def login(request: Request) -> Response:
+@application.post("/api/jwt-auth/")
+def login(request: Request) -> JsonResponse:
     user_data = request.json()
-    if user_data is None:
-        return Response(400, body="Bad request")
-
-    users.add(user_data["email"])
-    payload = {
-        "email": user_data["email"],
-        "exp": dt.datetime.utcnow() + dt.timedelta(seconds=JWT_EXP_DELTA_SECONDS),
-    }
-    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
-    return JsonResponse(data={"token": jwt_token})
+    email = user_data["email"]
+    users.add(email)
+    return JsonResponse(data={"token": users.gen_jwt(email)})
 
 
-@app.post("/api/notes")
-@authorization_required
-def add_note(request: Request) -> Response:
+@application.post("/api/notes")
+@users.protect()
+def add_note(request: Request) -> JsonResponse:
     note = request.json()
-    if note is None:
-        return Response(400, body="Bad request")
-
     note_id = len(notes) + 1
     note["id"] = note_id
     note["pub_date"] = dt.datetime.now()
@@ -76,42 +37,36 @@ def add_note(request: Request) -> Response:
     return JsonResponse(data=note, serializer=dt_json_serializer)
 
 
-@app.get("/api/notes")
-@authorization_required
+@application.get("/api/notes")
 def get_notes(request: Request) -> JsonResponse:
     notes_list = list(notes.values())
     return JsonResponse(data={"notes": notes_list}, serializer=dt_json_serializer)
 
 
-@app.get("/api/notes/{id:d}")
-@authorization_required
+@application.get("/api/notes/{id}")
 def get_note(request: Request, id: int) -> JsonResponse:
-    note_id = id
+    note_id = int(id)
     return JsonResponse(data=notes[note_id], serializer=dt_json_serializer)
 
 
-@app.patch("/api/notes/{id:d}")
-@authorization_required
-def update_note(request: Request, id: int) -> Response:
-    note_id = id
+@application.patch("/api/notes/{id}")
+def update_note(request: Request, id: int) -> JsonResponse:
+    note_id = int(id)
     data = request.json()
-    if data is None:
-        return Response(400, body="Bad request")
-
     note = notes[note_id]
     note["title"] = data["title"]
     note["body"] = data["body"]
     return JsonResponse(data={})
 
 
-app.add_middleware(CORSMiddleware)
+application.add_middleware(CORSMiddleware)
 
 
 def main():
     from wsgiserver import WSGIRequestHandler, WSGIServer
 
     server = WSGIServer(port=8080, request_handler_cls=WSGIRequestHandler)
-    server.set_app(app)
+    server.set_app(application)
     server.serve_forever()
 
 
